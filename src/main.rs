fe1d5ccf537da678490708da28d03a766cdb097e
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-04-27 04:57:43 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use rand::{SeedableRng, Rng};
+
+#[tokio::main]
+async fn main() {
+    let client = reqwest::ClientBuilder::new().redirect(reqwest::redirect::Policy::none()).build().unwrap();
+
+    const NUM_WORKERS: usize = 200;
+    let mut workers = Vec::new();
+
+    for _ in 0..NUM_WORKERS {
+        let client = client.clone();
+        workers.push(tokio::spawn(async move {
+            let mut rng = rand::rngs::SmallRng::from_entropy();
+
+            loop {
+                const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                let mut line = "https://git.io/".to_owned();
+
+                for _ in 0..5 {
+                    line.push(CHARSET[rng.gen_range(0..CHARSET.len())] as char);
+                }
+
+                let res: reqwest::Response = client.get(&line)
+                    .send()
+                    .await.unwrap();
+                if res.status().as_u16() == 404 {
+                    println!("{} -> 404", line);
+                } else if let Some(location) = res.headers().get("Location") {
+                    println!("{} -> {}", line, location.to_str().unwrap());
+                }
+                res.bytes().await.unwrap();
+            }
+        }));
+    }
+
+    for worker in workers {
+        worker.await.unwrap();
+    }
+}
